a226e68052542be9dee96ebc80e5da95cee2481f
selfup <boudinot.regis@yahoo.com>
2018-02-24 15:32:08 -0600

    add tests

--- src/main.rs
+++ src/main.rs
@@ -43,3 +43,27 @@ fn open_file_as_string(filename: &str) -> String {
 fn sub(source: &String, pattern: &str, replacement: &str) -> String {
     String::from(str::replace(&source, pattern, replacement))
 }
+
+#[test]
+fn it_stacks_lines_into_vec() {
+    let string_vec: Vec<String> = stack_lines_into_vec("./fixtures/test/test.csv");
+
+    assert_eq!("hello,", string_vec[0]);
+    assert_eq!("world,", string_vec[1]);
+    assert_eq!("foo,", string_vec[2]);
+    assert_eq!("bar", string_vec[3]);
+}
+
+#[test]
+fn it_opens_file_as_a_string() {
+    let string_file: String = open_file_as_string("./fixtures/test/test.csv");
+
+    assert_eq!("hello,\nworld,\nfoo,\nbar\n", string_file);
+}
+
+#[test]
+fn it_replaces_string_contents() {
+    let string: String = "hello\nworld\nfoo\nbar".to_string();
+
+    assert_eq!(String::from("helloworldfoobar"), sub(&string, "\n", ""));
+}
